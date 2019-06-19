abb885cf98bcd58d9f892ca9451937b86c8d4c11
GAONING-abc <1030364272@qq.com>
2019-06-19 22:02:12 +0800

    http

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,59 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 import 'navigator/tab_navigator.dart';
-
+import 'dart:convert';
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String showResult = '';
+  Future<CommonModel> fetchPost() async{
+    final response = await http.get('http://www.devio.org/io/flutter_app/json/test_common_model.json')
+    final result = json.decode(response.body);
+    return CommonModel.fromJson(result);
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('http'),
+        ),
+          body:Column()
       ),
-      home: TabNavigator(),
+
     );
   }
+}
+
+class CommonModel {
+  final String icon;
+  final String title;
+  final String url;
+  final String statusBarColor;
+  final bool hideAppBar;
+
+  CommonModel({this.icon, this.title, this.url, this.statusBarColor,
+    this.hideAppBar});
+
+  factory CommonModel.fromJson(Map<String, dynamic>json){
+    return CommonModel(
+      icon:
+      json['icon'];
+      title:
+      json['title'];
+      url:
+      json['url'];
+      statusBarColor:
+      json['statusBarColor'];
+      hideAppBar:
+      json['hideAppBar'];
+    );
+  }
+
 }
 
